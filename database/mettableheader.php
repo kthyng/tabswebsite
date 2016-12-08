0cd680216a1beb6a244600c2d641840982bdbc8b
Kristen Thyng <kthyng@gmail.com>
2016-12-08 16:32:56 -0600

    updated headers units

--- database/mettableheader.php
+++ database/mettableheader.php
@@ -1,3 +1,3 @@
 <b>   Date    Time    East    North    AirT  AtmPr    Gust    Comp    Tx   Ty    PAR   RelH   WSpeed    WDir</b>
-<i>           [UTC]   [m/s]   [m/s]    [&deg;C]   [mb]    [m/s]    [&deg;M]                     [%]    [m/s]    [From]</i>
-<hr WIDTH="800" ALIGN="LEFT" NOSHADE SIZE="1">
+<i>           UTC      m/s     m/s      &deg;C     mb      m/s     &deg;M                        %      m/s      From</i>
+<hr WIDTH="820" ALIGN="LEFT" NOSHADE SIZE="1">
--- database/ventableheader.php
+++ database/ventableheader.php
@@ -1,3 +1,3 @@
 <b>   Date       Time     East    North    Speed  Dir to   WaterT</b>
-<i>              [UTC]   [cm/s]  [cm/s]   [cm/s]   [&deg;T]     [&deg;C]</i>
+<i>              UTC      cm/s     cm/s     cm/s    &deg;T       &deg;C</i>
 <hr WIDTH="480" ALIGN="LEFT" NOSHADE SIZE="1">
