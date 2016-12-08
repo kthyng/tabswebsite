e135b0da53fbd2e9115aae7a28f5cfca06dc38ee
Kristen Thyng <kthyng@gmail.com>
2016-12-08 16:38:26 -0600

    added eng and salt headers

---
+++ database/engtableheader.php
@@ -0,0 +1,3 @@
+<b>   Date    Time    VBatt   SigStr  Comp    Nping  Tx   Ty   ADCP   ADCP   VBatt</b>
+<i>           UTC     Oper     dB      &deg;M                      Volt   Curr   sleep</i>
+<hr WIDTH="615" ALIGN="LEFT" NOSHADE SIZE="1">
---
+++ database/salttableheader.php
@@ -0,0 +1,3 @@
+<b>   Date     Time     Temp     Cond   Salinity   Density   SoundVel</b>
+<i>            UTC       &deg;C      ms/cm             kg/m^3     m/s </i>
+<hr WIDTH="480" ALIGN="LEFT" NOSHADE SIZE="1">
