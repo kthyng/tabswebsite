e83a735c3115c33940b448682b2356f019fd6818
Kristen Thyng <kthyng@gmail.com>
2016-12-08 16:21:58 -0600

    now can access met database through form

---
+++ database/mettableheader.php
@@ -0,0 +1,3 @@
+<b>   Date    Time    East    North    AirT  AtmPr    Gust    Comp    Tx   Ty    PAR   RelH   WSpeed    WDir</b>
+<i>           [UTC]   [m/s]   [m/s]    [&deg;C]   [mb]    [m/s]    [&deg;M]</i>                     [%]    [m/s]    [From]
+<hr WIDTH="800" ALIGN="LEFT" NOSHADE SIZE="1">
