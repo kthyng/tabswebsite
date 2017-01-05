3b9bd8218e538c5bfd31e29fab69816cca8a3124
Kristen Thyng <kthyng@gmail.com>
2017-01-05 14:02:55 -0600

    added in checks to include data when available

--- buoy_header.py
+++ buoy_header.py
@@ -2,6 +2,7 @@ import buoy_data
 import argparse
 import pandas as pd
 import plot_buoy
+import os
 
 # parse the input arguments
 parser = argparse.ArgumentParser()
@@ -9,7 +10,6 @@ parser.add_argument('buoy', type=str, help='which buoy')
 args = parser.parse_args()
 buoy = args.buoy
 
-# MAKE CURRENTS AND TEMP LARGE AND EVERYTHING ELSE SMALL
 # read in vel file
 which = 'ven'
 fname = 'daily/tabs_' + buoy + '_' + which
@@ -37,23 +37,29 @@ ping = tail['Nping'].values[0]
 # read in met file
 which = 'met'
 fname = 'daily/tabs_' + buoy + '_' + which
-df = plot_buoy.read(buoy, fname, which)
-tail = df.tail(1)
-# time = tail.index.strftime("%Y-%m-%d %H:%M")[0]
-wind = tail['Speed  [m/s]'].values[0]
-airtemp = tail['AirT  [deg  C]'].values[0]
-press = tail['AtmPr  [MB]'].values[0]
-wdirect = tail['Dir  from  [deg  T]'].values[0]
-humid = tail['RelH  [%]'].values[0]
+domet = False
+if os.path.exists(fname):
+    domet = True
+    df = plot_buoy.read(buoy, fname, which)
+    tail = df.tail(1)
+    # time = tail.index.strftime("%Y-%m-%d %H:%M")[0]
+    wind = tail['Speed  [m/s]'].values[0]
+    airtemp = tail['AirT  [deg  C]'].values[0]
+    press = tail['AtmPr  [MB]'].values[0]
+    wdirect = tail['Dir  from  [deg  T]'].values[0]
+    humid = tail['RelH  [%]'].values[0]
 
 # read in wave file
 which = 'wave'
 fname = 'daily/tabs_' + buoy + '_' + which
-df = plot_buoy.read(buoy, fname, which)
-tail = df.tail(1)
-# time = tail.index.strftime("%Y-%m-%d %H:%M")[0]
-wheight = tail['WaveHeight  [m]'].values[0]
-wperiod = tail['MeanPeriod  [s]'].values[0]
+dowave = False
+if os.path.exists(fname):
+    dowave = True
+    df = plot_buoy.read(buoy, fname, which)
+    tail = df.tail(1)
+    # time = tail.index.strftime("%Y-%m-%d %H:%M")[0]
+    wheight = tail['WaveHeight  [m]'].values[0]
+    wperiod = tail['MeanPeriod  [s]'].values[0]
 
 # buoy specs
 loc = buoy_data.locs(buoy)['lat'][0] + '&deg; ' +\
@@ -100,17 +106,20 @@ print('<td><small>Ping count: ' + str(ping) + '</small></td>')
 print('</tr>')
 
 # next row
-print('<tr>')
-print('<td><small>Wind: %2.2f (%2.2f kts) from %3.0f&deg;T (%s)</small></td>' % (wind, plot_buoy.convert(wind, 'mps2kts'), wdirect, plot_buoy.degrees_to_cardinal(wdirect)))
-print('<td><small>Air temp: %2.1f&deg;C (%2.0f&deg;F)</td>' % (airtemp, plot_buoy.convert(airtemp, 'c2f')))
-print('<td><small>Pressure: %4.1fmb (%2.2f inHg)</td>' % (press, plot_buoy.convert(press, 'mb2hg')))
-print('</tr>')
+if domet:
+    print('<tr>')
+    print('<td><small>Wind: %2.2f m/s (%2.2f kts) from %3.0f&deg;T (%s)</small></td>' % (wind, plot_buoy.convert(wind, 'mps2kts'), wdirect, plot_buoy.degrees_to_cardinal(wdirect)))
+    print('<td><small>Air temp: %2.1f&deg;C (%2.0f&deg;F)</td>' % (airtemp, plot_buoy.convert(airtemp, 'c2f')))
+    print('<td><small>Pressure: %4.1fmb (%2.2f inHg)</td>' % (press, plot_buoy.convert(press, 'mb2hg')))
+    print('</tr>')
 
-# next row
 print('<tr>')
+if domet:
+# next row
 # print('<td><small>&nbsp;&nbsp;&nbsp;  from %3.0f&deg;T (%s)</small></td>' % (wdirect, plot_buoy.degrees_to_cardinal(wdirect)))
-print('<td><small>Relative humidity: %2.0f&#37;</small></td>' % (humid))
-print('<td><small>Waves: %2.1fm (%2.1f ft) @ %2.1f sec</small></td>' % (wheight, plot_buoy.convert(wheight, 'm2ft'), wperiod))
+    print('<td><small>Relative humidity: %2.0f&#37;</small></td>' % (humid))
+if dowave:
+    print('<td><small>Waves: %2.1fm (%2.1f ft) @ %2.1f sec</small></td>' % (wheight, plot_buoy.convert(wheight, 'm2ft'), wperiod))
 print('</tr>')
 
 # next row
