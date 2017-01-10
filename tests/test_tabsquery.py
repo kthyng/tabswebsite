4bd0136b91cdf801d15ad5b40211a0b820ddfcc9
Kristen Thyng <kthyng@gmail.com>
2017-01-10 17:03:17 -0600

    have test for recent ven images appearing on link from front pages

--- tests/test_tabsquery.py
+++ tests/test_tabsquery.py
@@ -4,6 +4,7 @@ Test tabsquery.php
 
 import requests
 from bs4 import BeautifulSoup
+import buoy_data as bd
 
 
 base = 'http://localhost/tabswebsite/tabsquery.php?'
@@ -11,15 +12,21 @@ base = 'http://localhost/tabswebsite/tabsquery.php?'
 def test_recent():
     '''Test for all parts of call to "recent" showing up on tabsquery page.'''
 
-    buoy = 'V'
-    table = 'ven'
-    query = "Buoyname=" + buoy + "&table=" + table + "&Datatype=pic&datepicker=recent"
+    # buoy = 'V'
 
-    # read in website
-    out = requests.get(base + query)
-    # parse website
-    soup = BeautifulSoup(out.text, 'lxml')
-    # TEST FOR HEADER
-    # TEST THAT DOWNLOAD LINK WORKS (FILE EXISTS)
-    # is image there?
-    soup.findAll('a', href='daily/tabs_' + buoy + '_' + table + '.pdf')
+    for buoy in bd.buoys():
+        table = 'ven'
+        # for table in bd.tables():
+        if not buoy in bd.avail(table):
+            continue
+        query = "Buoyname=" + buoy + "&table=" + table + "&Datatype=pic&datepicker=recent"
+
+        # read in website
+        out = requests.get(base + query)
+        # parse website
+        soup = BeautifulSoup(out.text, 'lxml')
+        # TEST FOR HEADER
+        # TEST THAT DOWNLOAD LINK WORKS (FILE EXISTS)
+        # is image there?
+        assert soup.findAll('a', href='daily/tabs_' + buoy + '_' + table + '.pdf')
+        # assert result
