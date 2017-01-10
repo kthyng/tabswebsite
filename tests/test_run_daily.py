3702a5e43824a74f24d3536180c4f747b05a323b
Kristen Thyng <kthyng@gmail.com>
2017-01-10 12:58:57 -0600

    starting on tests for run_daily

---
+++ tests/test_run_daily.py
@@ -0,0 +1,26 @@
+'''
+Tests for run_daily.py
+'''
+
+
+
+def test_query_setup_recent():
+    '''Test query setup when easy.'''
+    pass
+
+
+def test_query_setup_recent_hard():
+    '''Test query setup when difficult.'''
+    pass
+
+
+def test_query_setup():
+    pass
+
+
+def test_make_text():
+    pass
+
+
+def test_overall():
+    pass
