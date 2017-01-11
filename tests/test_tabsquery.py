0a2cd833ac686dac78c3e9fb7606cc9d607b3d40
Kristen Thyng <kthyng@gmail.com>
2017-01-11 13:44:09 -0600

    added test for table presence from tabs query

--- tests/test_tabsquery.py
+++ tests/test_tabsquery.py
@@ -6,17 +6,18 @@ import requests
 from bs4 import BeautifulSoup
 import buoy_data as bd
 from os import path
+import re
 
 
 base = 'http://localhost/tabswebsite/tabsquery.php?'
 
-def test_recent():
-    '''Test for all parts of call (ven) to "recent" showing up on tabsquery page.'''
+def test_recent_image():
+    '''Test for all parts of call (ven) to "recent" showing up on tabsquery page.
+
+    This is just the image showing, but with the link to the data too.'''
 
     table = 'ven'
     for buoy in bd.buoys():
-        if not buoy in bd.avail(table):
-            continue
 
         query = "Buoyname=" + buoy + "&table=" + table + "&Datatype=pic&datepicker=recent"
 
@@ -26,9 +27,43 @@ def test_recent():
         soup = BeautifulSoup(out.text, 'lxml')
         # test for header
         assert soup.findAll('table', id="header")
-        # test for download link
-        assert soup.findAll('a', string='download')
         # test for download file
         assert path.exists('daily/tabs_' + buoy + '_' + table)
+        # test for download link
+        assert soup.findAll('a', string='download')
         # test for image
         assert soup.findAll('a', href='daily/tabs_' + buoy + '_' + table + '.pdf')
+
+
+def test_recent_table():
+    '''Test for all parts of "recent" to show a table.
+
+    This is just the table showing.'''
+
+    table = 'ven'
+    for buoy in bd.buoys():
+
+        query = "Buoyname=" + buoy + "&table=" + table + "&Datatype=data&datepicker=recent"
+
+        # read in website
+        out = requests.get(base + query)
+        # parse website
+        soup = BeautifulSoup(out.text, 'lxml')
+        # test for header
+        assert soup.findAll('table', id="header")
+        # test for download file
+        assert path.exists('daily/tabs_' + buoy + '_' + table)
+        # test for download link
+        assert soup.findAll('a', string='download')
+        # test for table
+        assert soup.findAll('table', id=re.compile("T_*"))
+
+
+def test_active_image():
+    '''Test that can query a particular date for all buoys for image.'''
+    pass
+
+
+def test_active_table():
+    '''Test that can query a particular date for all buoys for all available tables.'''
+    pass
