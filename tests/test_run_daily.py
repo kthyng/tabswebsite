f1c3a2fa07e8c171c15517a58e351676eec857b7
Kristen Thyng <kthyng@gmail.com>
2017-01-14 13:06:18 -0600

    updated test in run_daily for headers

--- tests/test_run_daily.py
+++ tests/test_run_daily.py
@@ -52,7 +52,7 @@ def test_made_ven():
         assert path.exists('daily/tabs_' + buoy + '_ven_low.png')
         assert path.exists('daily/tabs_' + buoy + '_ven.png')
         assert path.exists('daily/tabs_' + buoy + '_ven.pdf')
-        assert path.exists('daily/tabs_' + buoy + '_ven_header')
+        assert path.exists('daily/tabs_' + buoy + '_header')
 
 
 def test_overall():
