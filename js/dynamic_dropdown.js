f2a6595d71a3a00551007df2d4b6c448b51c9364
Kristen Thyng <kthyng@gmail.com>
2017-01-14 14:02:15 -0600

    cleaned up a little

--- js/dynamic_dropdown.js
+++ js/dynamic_dropdown.js
@@ -20,43 +20,43 @@ var data = {
     "X": ["ven", "eng", "met", "salt", "wave"],
 }
 
-    $(function() {
+$(function() {
 https://css-tricks.com/dynamic-dropdowns/
 http://jsfiddle.net/NaUAL/
 
-        $("#json-one").change(function() {
+    $("#json-one").change(function() {
 
-            var first = $(this),
-                second = $("#json-two"),
-                key = first.val(),
-                // instead of the original switch code
-                vals = data[key] == undefined ? data.base : data[key],
-                html = [];
-             // create insert html before adding
-             $.each(vals,function(i,val){
-                  if (val == "eng"){
-                    valname = "System data"
-                  }
-                  else if (val == "met"){
-                    valname = "Meteorological data"
-                  }
-                  else if (val == "ven"){
-                    valname = "Velocity data"
-                  }
-                  else if (val == "salt"){
-                    valname = "Water property data"
-                  }
-                  else if (val == "wave"){
-                    valname = "Wave data"
-                  }
-                  html.push('<option value=' + val + '>'+valname+'</option>')
-                // //   html.push('<option>'+val+'</option>')
-                //   html.push('<input type=radio name="table" value=' + val + ' checked>' + val)
+        var first = $(this),
+            second = $("#json-two"),
+            key = first.val(),
+            // instead of the original switch code
+            vals = data[key] == undefined ? data.base : data[key],
+            html = [];
+         // create insert html before adding
+         $.each(vals,function(i,val){
+              if (val == "eng"){
+                valname = "System data"
+              }
+              else if (val == "met"){
+                valname = "Meteorological data"
+              }
+              else if (val == "ven"){
+                valname = "Velocity data"
+              }
+              else if (val == "salt"){
+                valname = "Water property data"
+              }
+              else if (val == "wave"){
+                valname = "Wave data"
+              }
+              html.push('<option value=' + val + '>'+valname+'</option>')
+            // //   html.push('<option>'+val+'</option>')
+            //   html.push('<input type=radio name="table" value=' + val + ' checked>' + val)
 
-             });
-             // no need to empty the element before adding the new content
-             second.html(html.join());
-});
-
-
+         });
+         // no need to empty the element before adding the new content
+         second.html(html.join());
     });
+
+
+});
