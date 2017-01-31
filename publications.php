8f70bcd0904a2fc66fcfb6e77dbfe46b49a9f29b
Kristen Thyng <kthyng@gmail.com>
2017-01-31 14:14:55 -0600

    started list of publicatoins

---
+++ publications.php
@@ -0,0 +1,55 @@
+<head>
+    <title>Publications</title>
+</head>
+
+<div id="container">
+
+    <!-- include header from separate file -->
+    <?php include("includes/header.html");?>
+
+
+    <!-- include navigation from separate file -->
+    <?php include("includes/navigation.html");?>
+
+    <h2>TABS and Shelf Model Publications:</h2>
+    <ul style="list-style: none;">
+    <li>
+    Walpert, J. N., Guinasso, N. L., Lee III, L. L., &amp; Martin, R. D. (2011, September). Texas Automated Buoy System - Sustainable ocean observations to help protect the environment. In OCEANS 2011 (pp. 1-7). IEEE.
+    <a href=docs/TABS_Walpert_oceans2011.pdf>pdf</a>
+    </li>
+    <br>
+    <li>
+    Guinasso, N. L., Bender, L. C., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2009, October). Texas automated buoy system. In OCEANS 2009, MTS/IEEE Biloxi-Marine Technology for Our Future: Global and Local Challenges (pp. 1-7). IEEE.
+    <a href=docs/TABS_oceans2009.pdf>pdf</a>
+    </li>
+    <br>
+    <li>
+    Bender, L. C., Guinasso, N. L., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2007). Development, operation and results from the Texas Automated Buoy System. Gulf of Mexico Science, 25(1), 33.
+    <a href=docs/TABS_Descriptive_Paper.pdf>pdf</a>
+    </li>
+    <br>
+    <li>
+    Martin Jr, R. D., Guinasso Jr, N. L., Lee III, L. L., Walpert, J. N., Bender, L. C., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2005, May). TEN YEARS OF REALTIME, NEAR-SURFACE CURRENT OBSERVATIONS SUPPORTING OIL SPILL RESPONSE 1. In International Oil Spill Conference (Vol. 2005, No. 1, pp. 541-545). American Petroleum Institute.
+    <a href=docs/tabs10year.pdf>pdf</a>
+    </li>
+    <br>
+    <li>
+    Guinasso, N. L., Yip, J., Reid, R. O., Bender, L. C., Howard, M., Lee III, L. L., Walpert, J. N., Brooks, D. A., Hetland, R. D., &amp; Martin, R. D. (2001). Observing and Forecasting Coastal Currents: Texas Automated Buoy System (TABS). In OCEANS, 2001. MTS/IEEE Conference and Exhibition (Vol. 2, pp. 1318-1322). IEEE.
+    <a href=docs/oceans2001.pdf>pdf</a>
+    </li>
+
+
+        <!-- <l>
+        Guinasso, N. L., Yip, J., Reid, R. O., Bender, L. C., Howard, M., Lee, L. L., ... & Martin, R. D. (2001). Observing and Forecasting Coastal Currents: Texas Automated Buoy System (TABS). In OCEANS, 2001. MTS/IEEE Conference and Exhibition (Vol. 2, pp. 1318-1322). IEEE.
+        <a href=docs/TABS_Walpert_oceans2011.pdf>pdf</a>
+        </l>
+
+
+            <l>
+            Guinasso, N. L., Yip, J., Reid, R. O., Bender, L. C., Howard, M., Lee, L. L., ... & Martin, R. D. (2001). Observing and Forecasting Coastal Currents: Texas Automated Buoy System (TABS). In OCEANS, 2001. MTS/IEEE Conference and Exhibition (Vol. 2, pp. 1318-1322). IEEE.
+            <a href=docs/TABS_Descriptive_Paper.pdf>pdf</a>
+            </l> -->
+
+    </ul>
+
+</div>
