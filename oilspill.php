d4fab71f81f69c8954362b9c124764c789b9cc85
Kristen Thyng <kthyng@gmail.com>
2017-01-31 21:47:15 -0600

    added notes on oil spill stuff

---
+++ oilspill.php
@@ -0,0 +1,24 @@
+<head>
+    <title>Oil Spill Products</title>
+</head>
+
+<div id="container">
+
+    <!-- include header from separate file -->
+    <?php include("includes/header.html");?>
+
+
+    <!-- include navigation from separate file -->
+    <?php include("includes/navigation.html");?>
+
+    <h2>Oil Spill Data Products</h2>
+    <p>Oil spills can be tracked with NOAA's <a href=http://response.restoration.noaa.gov/oil-and-chemical-spills/oil-spills/response-tools/downloading-installing-and-running-gnome.html>GNOME</a> software. In GNOME, model output needs to be in particular form. Several model products in this form are available:</p>
+
+    <ul>
+    <li> Former Gulf-wide, wind-driven <a href=http://csanady.tamu.edu/GNOME/gnome2.html>TGLO/TABS model</a> forecast </li>
+    <li> TWDB estuaries and bays model <a href=http://midgewater.twdb.state.tx.us/bays_estuaries/GNOME/>output</a></li>
+    </ul>
+
+    <p>A newer, up-and-coming version of GNOME written in Python &ndash; <a href=http://noaa-orr-erd.github.io/PyGnome/>pyGNOME</a> &ndash; takes a broader range of input files. A summary of model output from the Texas shelf is <a href=http://pong.tamu.edu/oof_v2/main/download.php?lang=en>available</a>.
+
+</div>
