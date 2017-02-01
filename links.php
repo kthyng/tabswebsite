a4258a71991b8f1dea6ea559d1c240e4be292a98
Kristen Thyng <kthyng@gmail.com>
2017-01-31 21:23:58 -0600

    updated links to better match original site

--- links.php
+++ links.php
@@ -13,20 +13,18 @@
 
     <h2>Useful links:</h2>
     <ul>
-    <li> <a href=http://polar.ncep.noaa.gov/waves/viewer.shtml?-multi_1-latest-hs-gmex- style ="text-decoration:none">NOAA wave model</a></li>
-    <li> <a href="docs/vector_addition_trajectory_pub_final.xls" style ="text-decoration:none">Vector addition trajectory tool</a><!-- <img align=top src="images/icon_sm.png"> -->  </li>
-    <li> <a href="docs/notice_to_mariners_111810.pdf" style ="text-decoration:none">Notice to mariners</a> </li>
-    <li> <a href=http://gcoos.tamu.edu/ style ="text-decoration:none">Gulf of Mexico Coastal Ocean Observing System (GCOOS): Member</a> </li>
-    <li> <a href="http://www.glo.texas.gov/what-we-do/caring-for-the-coast/oil-spills/index.html" style ="text-decoration:none" >TGLO Oil Spill Division</a></a> </li>
-    <li> <a href=http://www.ndbc.noaa.gov style ="text-decoration:none">NDBC Home</A> </li>
-    <li> <a href=http://www.ndbc.noaa.gov/maps/WestGulf.shtml style ="text-decoration:none">NDBC West Gulf</a>  </li>
-    <li> <a href=http://midgewater.twdb.state.tx.us/bays_estuaries/tides.html style ="text-decoration:none">TWBD Tide Forecasts</a> </li>
-    <li> <a href=http://www.nhc.noaa.gov/ style ="text-decoration:none">National Hurricane Center</a>  </li>
-    <li> <a href=https://tidesandcurrents.noaa.gov/ports/index.html?port=hg style ="text-decoration:none">Houston/Galveston PORTS</a> </li>
-    <li> <a href=http://tpwd.texas.gov/>Texas Parks and Wildlife</a> </li>
-    <li> <a href=http://www.tpwd.state.tx.us/landwater/water/environconcerns/hab/ style ="text-decoration:none">Harmful Algal Blooms</a> </li>
-    <li> <a href=http://fermi.jhuapl.edu/avhrr/gm/ style ="text-decoration:none">AVHHR Images (John Hopkins)</a> </li>
-    <li> <a href=http://www.wavcis.lsu.edu/ style ="text-decoration:none">WAVCIS (LSU)</a> </li>
+    <li> NOAA <a href=http://polar.ncep.noaa.gov/waves/viewer.shtml?-multi_1-latest-hs-gmex->wave model</a></li>
+    <li> Vector addition <a href="docs/vector_addition_trajectory_pub_final.xls">trajectory tool</a><!-- <img align=top src="images/icon_sm.png"> -->  </li>
+    <li> <a href="docs/notice_to_mariners_111810.pdf">Notice</a> to mariners </li>
+    <li> Gulf of Mexico Coastal Ocean Observing System <a href=http://gcoos.tamu.edu/>(GCOOS)</a> Member </li>
+    <li> TGLO <a href="http://www.glo.texas.gov/what-we-do/caring-for-the-coast/oil-spills/index.html" >Oil Spill Division</a></a> </li>
+    <li> NDBC <a href=http://www.ndbc.noaa.gov>Home</a> and <a href=http://www.ndbc.noaa.gov/maps/WestGulf.shtml>West Gulf</a>  </li>
+    <li> TWBD <a href=http://midgewater.twdb.state.tx.us/bays_estuaries/tides.html>Tide Forecasts</a> </li>
+    <li> <a href=http://www.nhc.noaa.gov/>National Hurricane Center</a>  </li>
+    <li> Houston/Galveston <a href=https://tidesandcurrents.noaa.gov/ports/index.html?port=hg>PORTS</a> </li>
+    <li> Texas Parks and Wildlife <a href=http://www.tpwd.state.tx.us/landwater/water/environconcerns/hab/>Harmful Algal Blooms</a> </li>
+    <li> AVHHR <a href=http://fermi.jhuapl.edu/avhrr/gm/>Images</a> (John Hopkins) </li>
+    <li> <a href=http://www.wavcis.lsu.edu/>WAVCIS</a> (LSU) </li>
     </ul>
 
 </div>
