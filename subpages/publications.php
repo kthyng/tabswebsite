5f06a463a130c180014cded1f70666d8ba99bf55
Kristen Thyng <kthyng@gmail.com>
2017-05-22 15:35:48 -0500

    updated publications

--- subpages/publications.php
+++ subpages/publications.php
@@ -16,27 +16,27 @@
     <h3>TABS Publications:</h3>
     <ul style="list-style: none;">
     <li>
-    Walpert, J. N., Guinasso, N. L., Lee III, L. L., &amp; Martin, R. D. (2011, September). Texas Automated Buoy System - Sustainable ocean observations to help protect the environment. In OCEANS 2011 (pp. 1-7). IEEE.
+    Walpert, J. N., Guinasso, N. L., Lee III, L. L., &amp; Martin, R. D. (2011, September). Texas Automated Buoy System - Sustainable ocean observations to help protect the environment. In <i>OCEANS 2011</i> (pp. 1-7). IEEE.
     <a href=/tabswebsite/docs/TABS_Walpert_oceans2011.pdf>pdf</a>
     </li>
     <br>
     <li>
-    Guinasso, N. L., Bender, L. C., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2009, October). Texas automated buoy system. In OCEANS 2009, MTS/IEEE Biloxi-Marine Technology for Our Future: Global and Local Challenges (pp. 1-7). IEEE.
+    Guinasso, N. L., Bender, L. C., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2009, October). Texas automated buoy system. In <i>OCEANS 2009</i>, MTS/IEEE Biloxi-Marine Technology for Our Future: Global and Local Challenges (pp. 1-7). IEEE.
     <a href=/tabswebsite/docs/TABS_oceans2009.pdf>pdf</a>
     </li>
     <br>
     <li>
-    Bender, L. C., Guinasso, N. L., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2007). Development, operation and results from the Texas Automated Buoy System. Gulf of Mexico Science, 25(1), 33.
+    Bender, L. C., Guinasso, N. L., Walpert, J. N., Lee III, L. L., Martin, R. D., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2007). Development, operation and results from the Texas Automated Buoy System. <i>Gulf of Mexico Science</i>, 25(1), 33.
     <a href=/tabswebsite/docs/TABS_Descriptive_Paper.pdf>pdf</a>
     </li>
     <br>
     <li>
-    Martin Jr, R. D., Guinasso Jr, N. L., Lee III, L. L., Walpert, J. N., Bender, L. C., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2005, May). TEN YEARS OF REALTIME, NEAR-SURFACE CURRENT OBSERVATIONS SUPPORTING OIL SPILL RESPONSE 1. In International Oil Spill Conference (Vol. 2005, No. 1, pp. 541-545). American Petroleum Institute.
+    Martin Jr, R. D., Guinasso Jr, N. L., Lee III, L. L., Walpert, J. N., Bender, L. C., Hetland, R. D., Baum, S. K. &amp; Howard, M. K. (2005, May). TEN YEARS OF REALTIME, NEAR-SURFACE CURRENT OBSERVATIONS SUPPORTING OIL SPILL RESPONSE 1. In <i>International Oil Spill Conference</i> (Vol. 2005, No. 1, pp. 541-545). American Petroleum Institute.
     <a href=/tabswebsite/docs/tabs10year.pdf>pdf</a>
     </li>
     <br>
     <li>
-    Guinasso, N. L., Yip, J., Reid, R. O., Bender, L. C., Howard, M., Lee III, L. L., Walpert, J. N., Brooks, D. A., Hetland, R. D., &amp; Martin, R. D. (2001). Observing and Forecasting Coastal Currents: Texas Automated Buoy System (TABS). In OCEANS, 2001. MTS/IEEE Conference and Exhibition (Vol. 2, pp. 1318-1322). IEEE.
+    Guinasso, N. L., Yip, J., Reid, R. O., Bender, L. C., Howard, M., Lee III, L. L., Walpert, J. N., Brooks, D. A., Hetland, R. D., &amp; Martin, R. D. (2001). Observing and Forecasting Coastal Currents: Texas Automated Buoy System (TABS). In <i>OCEANS, 2001</i>. MTS/IEEE Conference and Exhibition (Vol. 2, pp. 1318-1322). IEEE.
     <a href=/tabswebsite/docs/oceans2001.pdf>pdf</a>
     </li>
     </ul>
@@ -46,37 +46,42 @@
     <ul style="list-style: none;">
 
     <li>
-    Bacosa, H. P., Thyng, K. M., Plunkett, S., Erdner, D. L., &amp; Liu, Z. (2016). The tarballs on Texas beaches following the 2014 Texas City "Y" Spill: Modeling, chemical, and microbiological studies. Marine Pollution Bulletin, 109(1), 236&ndash;244, doi:10.1016/j.marpolbul.2016.05.076.
+    Thyng, K. M., &amp; Hetland, R. D. (2017). Texas and Louisiana coastal vulnerability and shelf connectivity. <i>Marine Pollution Bulletin</i>, Volume 116, Issues 1&ndash;2, Pages 226&ndash;233. doi:10.1016/j.marpolbul.2016.12.074.
+    <a href=http://www.sciencedirect.com/science/article/pii/S0025326X16310773>link</a>
+    </li> <br>
+
+    <li>
+    Bacosa, H. P., Thyng, K. M., Plunkett, S., Erdner, D. L., &amp; Liu, Z. (2016). The tarballs on Texas beaches following the 2014 Texas City "Y" Spill: Modeling, chemical, and microbiological studies. <i>Marine Pollution Bulletin</i>, 109(1), 236&ndash;244, doi:10.1016/j.marpolbul.2016.05.076.
     <a href=http://doi.org/10.1016/j.marpolbul.2016.05.076>link</a>
     </li> <br>
 
     <li>
-    Marta-Almeida, M., Hetland, R. D., &amp; Zhang, X. (2013). Evaluation of model nesting performance on the Texas-Louisiana continental shelf. Journal of Geophysical Research: Oceans, 118(5), 2476&ndash;2491, doi:10.1002/jgrc.20163.
+    Marta-Almeida, M., Hetland, R. D., &amp; Zhang, X. (2013). Evaluation of model nesting performance on the Texas-Louisiana continental shelf. <i>Journal of Geophysical Research: Oceans</i>, 118(5), 2476&ndash;2491, doi:10.1002/jgrc.20163.
     <a href=http://doi.org/10.1002/jgrc.20163>link</a>
     </li><br>
 
     <li>
-    Thyng, K. M., Hetland, R. D., Ogle, M. T., Zhang, X., Chen, F., &amp; Campbell, L. (2013). Origins of <i>Karenia brevis</i> harmful algal blooms along the Texas coast. Limnology and Oceanography: Fluids and Environments, 3(1), 269-278, doi:10.1215/21573689-2417719.
+    Thyng, K. M., Hetland, R. D., Ogle, M. T., Zhang, X., Chen, F., &amp; Campbell, L. (2013). Origins of <i>Karenia brevis</i> harmful algal blooms along the Texas coast. <i>Limnology and Oceanography: Fluids and Environments</i>, 3(1), 269-278, doi:10.1215/21573689-2417719.
     <a href=http://doi.org/10.1215/21573689-2417719>link</a>
     </li><br>
 
     <li>
-    Zhang, X., Hetland, R. D., Marta-Almeida, M., &amp; DiMarco, S. F. (2012). A numerical investigation of the Mississippi and Atchafalaya freshwater transport, filling and flushing times on the Texas-Louisiana Shelf. Journal of Geophysical Research, 117(C11), C11009, doi:10.1029/2012JC008108.
+    Zhang, X., Hetland, R. D., Marta-Almeida, M., &amp; DiMarco, S. F. (2012). A numerical investigation of the Mississippi and Atchafalaya freshwater transport, filling and flushing times on the Texas-Louisiana Shelf. <i>Journal of Geophysical Research: Oceans</i>, 117(C11), C11009, doi:10.1029/2012JC008108.
     <a href=http://doi.org/10.1029/2012JC008108>link</a>
     </li> <br>
 
     <li>
-    Zhang, X., Marta-Almeida, M., &amp; Hetland, R. D. (2012). A high-resolution pre-operational forecast model of circulation on the Texas-Louisiana continental shelf and slope. Journal of Operational Oceanography, 5(1), 19&ndash;34, doi:10.1080/1755876X.2012.11020129.
+    Zhang, X., Marta-Almeida, M., &amp; Hetland, R. D. (2012). A high-resolution pre-operational forecast model of circulation on the Texas-Louisiana continental shelf and slope. <i>Journal of Operational Oceanography</i>, 5(1), 19&ndash;34, doi:10.1080/1755876X.2012.11020129.
     <a href=http://dx.doi.org/10.1080/1755876X.2012.11020129>link</a>
     </li> <br>
 
     <li>
-    Zhang, Z., &amp; Hetland, R. (2012). A numerical study on convergence of alongshore flows over the Texas-Louisiana shelf. Journal of Geophysical Research, 117(C11), C11010, doi:10.1029/2012JC008145.
+    Zhang, Z., &amp; Hetland, R. (2012). A numerical study on convergence of alongshore flows over the Texas-Louisiana shelf. <i>Journal of Geophysical Research: Oceans</i>, 117(C11), C11010, doi:10.1029/2012JC008145.
     <a href=http://doi.org/10.1029/2012JC008145>link</a>
     </li><br>
 
     <li>
-    Hetland, R. D., &amp; Campbell, L. (2007). Convergent blooms of <i>Karenia brevis</i> along the Texas coast. Geophysical Research Letters, 34(19), doi:10.1029/2007GL030474.
+    Hetland, R. D., &amp; Campbell, L. (2007). Convergent blooms of <i>Karenia brevis</i> along the Texas coast. <i>Geophysical Research Letters</i>, 34(19), doi:10.1029/2007GL030474.
     <a href=http://doi.org/10.1029/2007GL030474>link</a>
     </li> <br>
 
@@ -85,7 +90,7 @@
     <h3>Other:</h3>
     <ul style="list-style: none;">
     <li>
-    Thyng, K. M., C. A. Greene, R. D. Hetland, H. M. Zimmerle, and S. F. DiMarco (2016), True colors  of  oceanography:  Guidelines  for  effective  and  accurate  colormap  selection, Oceanography, 29(3), 9&ndash;13, doi:10.5670/oceanog.2016.66.
+    Thyng, K. M., C. A. Greene, R. D. Hetland, H. M. Zimmerle, and S. F. DiMarco (2016), True colors  of  oceanography:  Guidelines  for  effective  and  accurate  colormap  selection, <i>Oceanography</i>, 29(3), 9&ndash;13, doi:10.5670/oceanog.2016.66.
     <a href=http://doi.org/10.5670/oceanog.2016.66 >link</a>
     </li>
     </ul>
