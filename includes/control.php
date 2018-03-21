41eef288fb94665b45fa0159f26e19f7148d18e7
Kristen Thyng <kthyng@gmail.com>
2018-03-21 14:45:14 -0500

    removed 3 inactive buoys and made 2 more lists of buoys read from csv file

--- includes/control.php
+++ includes/control.php
@@ -32,90 +32,26 @@ print "<td>Update results: </td>";
 // print "<br> &nbsp;\n";
 print "<br><form action=\"/tabswebsite/subpages/tabsquery.php\" method=\"get\">\n";
 
+// read into an array the active buoys
+// read in buoy list from csv file with buoy info
+$csv = array_map("str_getcsv", file("../includes/buoys.csv"));
+$header = array_shift($csv); // Seperate the header from data
+$col = array_search("buoy", $header);  # save column name for buoys
+$active = array_search("active", $header);  # save column name for buoys being active
+foreach ($csv as $row) {  // loop over each row in csv file
+    // check if buoy is active
+    if (strcmp($row[$active], "TRUE") == 0) {
+    	$blet[] = $row[$col];  // if so, save buoy name
+    }
+}
+
 // Change buoy
 print "<td><Select Name=Buoyname id=json-one>\n";
 print "<OPTION SELECTED value='$Buoyname'>$Buoyname</option>\n";
-print "<option value='B'>B</option>\n";
-print "<option value='D'>D</option>\n";
-print "<option value='F'>F</option>\n";
-print "<option value='J'>J</option>\n";
-print "<option value='K'>K</option>\n";
-print "<option value='R'>R</option>\n";
-print "<option value='V'>V</option>\n";
-print "<option value='W'>W</option>\n";
-print "<option value='X'>X</option>\n";
-print "<OPTION value='42001'>42001</option>\n";
-print "<OPTION value='42002'>42002</option>\n";
-print "<OPTION value='42019'>42019</option>\n";
-print "<OPTION value='42020'>42020</option>\n";
-print "<OPTION value='42035'>42035</option>\n";
-print "<OPTION value='42036'>42036</option>\n";
-print "<OPTION value='42039'>42039</option>\n";
-print "<OPTION value='42040'>42040</option>\n";
-print "<OPTION value='SRST2'>SRST2</option>\n";
-print "<OPTION value='PTAT2'>PTAT2</option>\n";
-print "<OPTION value='BURL1'>BURL1</option>\n";
-print "<OPTION value='g06010'>g06010</option>\n";
-print "<OPTION value='mc0101'>mc0101</option>\n";
-print "<OPTION value='sn0101'>sn0101</option>\n";
-print "<OPTION value='sn0201'>sn0201</option>\n";
-print "<OPTION value='sn0301'>sn0301</option>\n";
-print "<OPTION value='sn0401'>sn0401</option>\n";
-print "<OPTION value='sn0501'>sn0501</option>\n";
-print "<OPTION value='sn0701'>sn0701</option>\n";
-print "<OPTION value='lc0101'>lc0101</option>\n";
-print "<OPTION value='lc0201'>lc0201</option>\n";
-print "<OPTION value='mg0101'>mg0101</option>\n";
-print "<OPTION value='8770475'>8770475</option>\n";
-print "<OPTION value='8770520'>8770520</option>\n";
-print "<OPTION value='8770777'>8770777</option>\n";
-print "<OPTION value='8770808'>8770808</option>\n";
-print "<OPTION value='8770822'>8770822</option>\n";
-print "<OPTION value='8770971'>8770971</option>\n";
-print "<OPTION value='8771486'>8771486</option>\n";
-print "<OPTION value='8771972'>8771972</option>\n";
-print "<OPTION value='8772985'>8772985</option>\n";
-print "<OPTION value='8773037'>8773037</option>\n";
-print "<OPTION value='8773146'>8773146</option>\n";
-print "<OPTION value='8773259'>8773259</option>\n";
-print "<OPTION value='8773701'>8773701</option>\n";
-print "<OPTION value='8774230'>8774230</option>\n";
-print "<OPTION value='8775237'>8775237</option>\n";
-print "<OPTION value='8775241'>8775241</option>\n";
-print "<OPTION value='8775244'>8775244</option>\n";
-print "<OPTION value='8775296'>8775296</option>\n";
-print "<OPTION value='8775792'>8775792</option>\n";
-print "<OPTION value='8776139'>8776139</option>\n";
-print "<OPTION value='8776604'>8776604</option>\n";
-print "<OPTION value='8777812'>8777812</option>\n";
-print "<OPTION value='8778490'>8778490</option>\n";
-print "<OPTION value='8779280'>8779280</option>\n";
-print "<OPTION value='8779748'>8779748</option>\n";
-print "<OPTION value='8779749'>8779749</option>\n";
-print "<OPTION value='8734673'>8734673</option>\n";
-print "<OPTION value='8735180'>8735180</option>\n";
-print "<OPTION value='8741003'>8741003</option>\n";
-print "<OPTION value='8741041'>8741041</option>\n";
-print "<OPTION value='8741094'>8741094</option>\n";
-print "<OPTION value='8741501'>8741501</option>\n";
-print "<OPTION value='8741533'>8741533</option>\n";
-print "<OPTION value='8747437'>8747437</option>\n";
-print "<OPTION value='8760721'>8760721</option>\n";
-print "<OPTION value='8760922'>8760922</option>\n";
-print "<OPTION value='8761305'>8761305</option>\n";
-print "<OPTION value='8761724'>8761724</option>\n";
-print "<OPTION value='8764227'>8764227</option>\n";
-print "<OPTION value='8764314'>8764314</option>\n";
-print "<OPTION value='8766072'>8766072</option>\n";
-print "<OPTION value='8768094'>8768094</option>\n";
-print "<OPTION value='8770570'>8770570</option>\n";
-print "<OPTION value='8770613'>8770613</option>\n";
-print "<OPTION value='8771013'>8771013</option>\n";
-print "<OPTION value='8771341'>8771341</option>\n";
-print "<OPTION value='8771450'>8771450</option>\n";
-print "<OPTION value='8773767'>8773767</option>\n";
-print "<OPTION value='8775870'>8775870</option>\n";
-print "<OPTION value='8779770'>8779770</option></select></td>\n";
+foreach ($blet as $f) {
+    print "<option value='$f'>$f</option>\n";
+}
+print "</select></td>\n";
 
 // Change table (variable file)
 print "<td><select id=json-two name=table>";
--- subpages/tabsqueryform.php
+++ subpages/tabsqueryform.php
@@ -37,6 +37,23 @@ else if ($units == 'E') {
     $unitsname = 'English';
 }
 
+// read into an array the active buoys
+$csv = array_map("str_getcsv", file("../includes/buoys.csv"));
+$header = array_shift($csv); // Seperate the header from data
+$col = array_search("buoy", $header);  # save column name for buoys
+$active = array_search("active", $header);  # save column name for buoys being active
+foreach ($csv as $row) {  // loop over each row in csv file
+    // check if buoy is active
+    if (strcmp($row[$active], "TRUE") == 0) {
+    	$buoysa[] = $row[$col];  // if so, save buoy name
+    }
+    // check if buoy is inactive
+    if (strcmp($row[$active], "FALSE") == 0) {
+    	$buoysia[] = $row[$col];  // if so, save buoy name
+    }
+}
+
+
 include("../includes/header.html");
 include("../includes/navigation.html");
 
@@ -52,106 +69,19 @@ print <<<_HTML_
 <TD><B>Select Buoy:</B> (* inactive)</TD>
 <TD><SELECT name="Buoyname" id="json-one">
 <OPTION SELECTED value="$buoy">$buoy
-<OPTION value="B">B
-<OPTION value="D">D
-<OPTION value="F">F
-<OPTION value="J">J
-<OPTION value="K">K
-<OPTION value="R">R
-<OPTION value="V">V
-<OPTION value="W">W
-<OPTION value="X">X
-<OPTION value="42001">42001
-<OPTION value="42002">42002
-<OPTION value="42019">42019
-<OPTION value="42020">42020
-<OPTION value="42035">42035
-<OPTION value="42036">42036
-<OPTION value="42039">42039
-<OPTION value="42040">42040
-<OPTION value="SRST2">SRST2
-<OPTION value="PTAT2">PTAT2
-<OPTION value="BURL1">BURL1
-<OPTION value="g06010">g06010
-<OPTION value="mc0101">mc0101
-<OPTION value="sn0101">sn0101
-<OPTION value="sn0201">sn0201
-<OPTION value="sn0301">sn0301
-<OPTION value="sn0401">sn0401
-<OPTION value="sn0501">sn0501
-<OPTION value="sn0701">sn0701
-<OPTION value="lc0101">lc0101
-<OPTION value="lc0201">lc0201
-<OPTION value="mg0101">mg0101
-<OPTION value='8770475'>8770475
-<OPTION value='8770520'>8770520
-<OPTION value='8770777'>8770777
-<OPTION value='8770808'>8770808
-<OPTION value='8770822'>8770822
-<OPTION value='8770971'>8770971
-<OPTION value='8771486'>8771486
-<OPTION value='8771972'>8771972
-<OPTION value='8772985'>8772985
-<OPTION value='8773037'>8773037
-<OPTION value='8773146'>8773146
-<OPTION value='8773259'>8773259
-<OPTION value='8773701'>8773701
-<OPTION value='8774230'>8774230
-<OPTION value='8775237'>8775237
-<OPTION value='8775241'>8775241
-<OPTION value='8775244'>8775244
-<OPTION value='8775296'>8775296
-<OPTION value='8775792'>8775792
-<OPTION value='8776139'>8776139
-<OPTION value='8776604'>8776604
-<OPTION value='8777812'>8777812
-<OPTION value='8778490'>8778490
-<OPTION value='8779280'>8779280
-<OPTION value='8779748'>8779748
-<OPTION value='8779749'>8779749
-<OPTION value='8734673'>8734673
-<OPTION value='8735180'>8735180
-<OPTION value='8741003'>8741003
-<OPTION value='8741041'>8741041
-<OPTION value='8741094'>8741094
-<OPTION value='8741501'>8741501
-<OPTION value='8741533'>8741533
-<OPTION value='8747437'>8747437
-<OPTION value='8760721'>8760721
-<OPTION value='8760922'>8760922
-<OPTION value='8761305'>8761305
-<OPTION value='8761724'>8761724
-<OPTION value='8764227'>8764227
-<OPTION value='8764314'>8764314
-<OPTION value='8766072'>8766072
-<OPTION value='8768094'>8768094
-<OPTION value='8770570'>8770570
-<OPTION value='8770613'>8770613
-<OPTION value='8771013'>8771013
-<OPTION value='8771341'>8771341
-<OPTION value='8771450'>8771450
-<OPTION value='8773767'>8773767
-<OPTION value='8775870'>8775870
-<OPTION value='8779770'>8779770
-<OPTION value="A">A*
-<OPTION value="C">C*
-<OPTION value="E">E*
-<OPTION value="G">G*
-<OPTION value="H">H*
-<OPTION value="N">N*
-<OPTION value="P">P*
-<OPTION value="S">S*
-<OPTION value="42007">42007*
-<OPTION value="ps0201">ps0201*
-<OPTION value="ps0301">ps0301*
-<OPTION value="ps0401">ps0401*
-<OPTION value="g09010">g09010*
-<OPTION value='8770733'>8770733*
-<OPTION value='8774513'>8774513*
-<OPTION value='8775283'>8775283*
-</SELECT>
-</TD>
+_HTML_;
 
+// list active bouys
+foreach ($buoysa as $b) {
+    print "<option value='$b'>$b</option>\n";
+}
+// then list inactive buoys
+foreach ($buoysia as $b) {
+    print "<option value='$b'>$b*</option>\n";
+}
+print "</select></td>\n";
+
+print <<<_HTML_
 
 <TD>
 <B>... then select dataset&nbsp;</B>
