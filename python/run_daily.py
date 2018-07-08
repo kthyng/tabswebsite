56a7d27f31d46cdce36efe97bbbb91306214be8c
Kristen Thyng <kthyng@gmail.com>
2018-07-08 11:29:04 -0500

    tweak so run_daily should not stop if problem with a buoy

--- python/run_daily.py
+++ python/run_daily.py
@@ -45,85 +45,88 @@ if __name__ == "__main__":
             if not bys[buoy]['active']:
                 continue
             # print(buoy)
-            # read in data in UTC
-            if bys[buoy]['inmysql']:  # mysql tables
-                if table == 'sum':
-                    # need to have this choose most recent data available
-                    # choose to look for ven since sum mostly shows ven data
-                    dend = tools.query_setup_recent(engine, buoy, 'ven').tz_localize('utc')
+            try:
+                # read in data in UTC
+                if bys[buoy]['inmysql']:  # mysql tables
+                    if table == 'sum':
+                        # need to have this choose most recent data available
+                        # choose to look for ven since sum mostly shows ven data
+                        dend = tools.query_setup_recent(engine, buoy, 'ven').tz_localize('utc')
+                    else:
+                        dend = tools.query_setup_recent(engine, buoy, table).tz_localize('utc')
                 else:
-                    dend = tools.query_setup_recent(engine, buoy, table).tz_localize('utc')
-            else:
-                dend = pd.Timestamp('now', tz='utc')
-            # start 5 days earlier from 00:00 on day of last data, and account for time zones
-            # so that enough data is read in for time zone conversion
-            tzoffset = (dend.tz_localize(None) - dend.tz_convert(tz).tz_localize(None)).seconds/3600.
-            dstart = (dend - pd.Timedelta('5 days')).normalize() + pd.Timedelta(str(tzoffset) + ' hours')
-            dend += pd.Timedelta(str(tzoffset) + ' hours')
-            df = read.read(buoy, dstart, dend, table=table, usemodel=False,
-                           userecent=True, tz=tz)
-            if len(buoy) == 1:
-                fname = path.join('..', 'daily', 'tabs_' + buoy + '_' + table)
-            else:
-                fname = path.join('..', 'daily', buoy)
-            # write daily data file, for whatever most recent time period
-            # data was available
-            if df is not None:
-                tools.write_file(df, fname)
-            # if there are too few rows to plot, set as None
-            if df is not None and len(df) < 2:
-                df = None
-            # no model output for stations in bays or outside domain
-            now = pd.Timestamp('now', tz='utc').normalize()
-            past = now - pd.Timedelta('5 days')
-            future = now + pd.Timedelta('4 days')
-            if bp.model(buoy, 'rho') and table != 'eng':
-                # read in recent model output, not tied to when data output was found
-                dfmodelrecent = read.read(buoy, past, now, table=table,
-                                                usemodel='recent', tz=tz)
-                # read in forecast model output, not tied to when data output was found
-                # subtract a day so that models overlap for sure
-                dfmodelforecast = read.read(buoy, now - pd.Timedelta('1 day'),
-                                            future, table=table,
-                                            usemodel='forecast', tz=tz)
-                # Catch if model output isn't working
-                if dfmodelrecent is None or dfmodelforecast is None:
-                    eflag = True
-            else:
-                dfmodelrecent = None
-                dfmodelforecast = None
+                    dend = pd.Timestamp('now', tz='utc')
+                # start 5 days earlier from 00:00 on day of last data, and account for time zones
+                # so that enough data is read in for time zone conversion
+                tzoffset = (dend.tz_localize(None) - dend.tz_convert(tz).tz_localize(None)).seconds/3600.
+                dstart = (dend - pd.Timedelta('5 days')).normalize() + pd.Timedelta(str(tzoffset) + ' hours')
+                dend += pd.Timedelta(str(tzoffset) + ' hours')
+                df = read.read(buoy, dstart, dend, table=table, usemodel=False,
+                               userecent=True, tz=tz)
+                if len(buoy) == 1:
+                    fname = path.join('..', 'daily', 'tabs_' + buoy + '_' + table)
+                else:
+                    fname = path.join('..', 'daily', buoy)
+                # write daily data file, for whatever most recent time period
+                # data was available
+                if df is not None:
+                    tools.write_file(df, fname)
+                # if there are too few rows to plot, set as None
+                if df is not None and len(df) < 2:
+                    df = None
+                # no model output for stations in bays or outside domain
+                now = pd.Timestamp('now', tz='utc').normalize()
+                past = now - pd.Timedelta('5 days')
+                future = now + pd.Timedelta('4 days')
+                if bp.model(buoy, 'rho') and table != 'eng':
+                    # read in recent model output, not tied to when data output was found
+                    dfmodelrecent = read.read(buoy, past, now, table=table,
+                                                    usemodel='recent', tz=tz)
+                    # read in forecast model output, not tied to when data output was found
+                    # subtract a day so that models overlap for sure
+                    dfmodelforecast = read.read(buoy, now - pd.Timedelta('1 day'),
+                                                future, table=table,
+                                                usemodel='forecast', tz=tz)
+                    # Catch if model output isn't working
+                    if dfmodelrecent is None or dfmodelforecast is None:
+                        eflag = True
+                else:
+                    dfmodelrecent = None
+                    dfmodelforecast = None
 
-            if bys[buoy]['table2'] == 'tidepredict' or bys[buoy]['table2'] == 'currentspredict':
-                # import pdb; pdb.set_trace()
-                dfmodeltides = read.read(buoy, past, future, usemodel=True, tz=tz)
-            else:
-                dfmodeltides = None
+                if bys[buoy]['table2'] == 'tidepredict' or bys[buoy]['table2'] == 'currentspredict':
+                    # import pdb; pdb.set_trace()
+                    dfmodeltides = read.read(buoy, past, future, usemodel=True, tz=tz)
+                else:
+                    dfmodeltides = None
 
-            # extra time needed in x direction
-            if dfmodeltides is not None:  # catches PORTS and tide prediction
-                tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(future).to_pydatetime())]
-            # none of these use model output, so no forecast and therefore no
-            elif table == 'wave' or table == 'eng' or not bp.model(buoy, 'rho'):
-                # tlims = None
-                tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(now).to_pydatetime())]
-            else:
-                # tlims = [dfmodelrecent['idx'].iloc[0], dfmodelforecast['idx'].iloc[-1]]
-                tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(future).to_pydatetime())]
+                # extra time needed in x direction
+                if dfmodeltides is not None:  # catches PORTS and tide prediction
+                    tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(future).to_pydatetime())]
+                # none of these use model output, so no forecast and therefore no
+                elif table == 'wave' or table == 'eng' or not bp.model(buoy, 'rho'):
+                    # tlims = None
+                    tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(now).to_pydatetime())]
+                else:
+                    # tlims = [dfmodelrecent['idx'].iloc[0], dfmodelforecast['idx'].iloc[-1]]
+                    tlims = [date2num(pd.to_datetime(past).to_pydatetime()), date2num(pd.to_datetime(future).to_pydatetime())]
 
-            # will plot model output from now if available
-            # make figure if any df is not equal to None
-            if any([dft is not None for dft in [df, dfmodelrecent, dfmodelforecast,dfmodeltides]]):
-                fig = plot_buoy.plot(df, buoy, table, df1=None, df2=dfmodelrecent,
-                                     df3=dfmodelforecast, df4=dfmodeltides,
-                                     tlims=tlims)
-                fig.savefig(fname + '.pdf')
-                fig.savefig(fname + '.png')
-                # save smaller for hover
-                fig.savefig(fname + '_low.png', dpi=60)
-                close(fig)
-            else:
-                logging.warning('No figure was created for buoy %s (table %s)' % (buoy, table))
-
+                # will plot model output from now if available
+                # make figure if any df is not equal to None
+                if any([dft is not None for dft in [df, dfmodelrecent, dfmodelforecast,dfmodeltides]]):
+                    fig = plot_buoy.plot(df, buoy, table, df1=None, df2=dfmodelrecent,
+                                         df3=dfmodelforecast, df4=dfmodeltides,
+                                         tlims=tlims)
+                    fig.savefig(fname + '.pdf')
+                    fig.savefig(fname + '.png')
+                    # save smaller for hover
+                    fig.savefig(fname + '_low.png', dpi=60)
+                    close(fig)
+                else:
+                    logging.warning('No figure was created for buoy %s (table %s)' % (buoy, table))
+            except Exception as e:
+                logging.exception(e)
+                logging.warning('Problem reading in data or model for buoy %s (table %s)' % (buoy, table))
 
     engine.dispose()
 
