807384eec84d30843cc9960f255ef61c13b9e760
Kristen Thyng <kthyng@gmail.com>
2020-09-16 16:08:56 -0500

    Updated model station files

--- python/read.py
+++ python/read.py
@@ -626,18 +626,14 @@ def read_model(buoy, which, dstart, dend, timing='recent', units='Metric',
     if timing == 'hindcast':
         locs = ['http://barataria.tamu.edu:8080/thredds/dodsC/NcML/txla_hindcast_sta',
                 'http://copano.tamu.edu:8080/thredds/dodsC/NcML/txla_hindcast_sta',
-                'http://terrebonne.tamu.edu:8080/thredds/dodsC/NcML/txla_hindcast_sta_agg',
                 'http://barataria.tamu.edu:6060/thredds/dodsC/NcML/txla_hindcast_sta']
     elif timing == 'recent':
-        locs = ['http://terrebonne.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive_agg.nc',
-                'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive_agg.nc',
-                'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive2_agg.nc',
-                'http://terrebonne.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive2_agg.nc']
+        locs = ['http://barataria.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive_agg.nc',
+                'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/forecast_stn_archive2_agg.nc']
     elif timing == 'forecast':
-        locs = ['http://terrebonne.tamu.edu:8080/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc',
-                'http://copano.tamu.edu:8080/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc',
-                'http://barataria.tamu.edu:8080/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc',
-                'http://barataria.tamu.edu:6060/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc']
+        locs = ['http://barataria.tamu.edu:8080/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc',
+                'http://barataria.tamu.edu:6060/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc',
+                'http://copano.tamu.edu:8080/thredds/dodsC/forecast_latest/txla2_stn_f_latest.nc']
 
 
     varstot = ['u', 'v', 'temp', 'salt', 'dye_01', 'dye_02', 'dye_03', 'dye_04',
